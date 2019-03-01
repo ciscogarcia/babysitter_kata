c8621a6fbe31999c8ccd94bcdd0c577ae51d95b7
Cisco Garcia <cisco.garcia@helloworld.com>
2019-03-01 15:09:11 -0500

    test that the babysitter has not babysat tonight

--- babysitter.py
+++ babysitter.py
@@ -3,6 +3,7 @@ class Babysitter():
         self.start_time = start_time
         self.end_time = end_time
         self.family = family
+        self.has_babysat_tonight = False
         self.times = ["5p", "6p", "7p", "8p", "9p", "10p", "11p", "12a", "1a", "2a", "3a", "4a"]
 
     def is_valid_start_time(self):
@@ -14,3 +15,6 @@ class Babysitter():
         if self.end_time in self.times:
             return True
         return False
+
+    def has_not_babysat_tonight(self):
+        return self.has_babysat_tonight
--- test_babysitter.py
+++ test_babysitter.py
@@ -11,3 +11,6 @@ class TestBabysitter(unittest.TestCase):
 
     def test_starts_no_later_than_4_am(self):
         self.assertEqual(True, self.b.is_valid_end_time())
+
+    def test_only_babysits_for_1_family__per_night(self):
+        self.assertEqual(False, self.b.has_not_babysat_tonight())
