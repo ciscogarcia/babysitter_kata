f42150cb8e105612db255aed224bf8a747aef5bb
Cisco Garcia <cisco.garcia@helloworld.com>
2019-03-01 14:53:13 -0500

    pull babysitter instantiantion into setup fixture

--- test_babysitter.py
+++ test_babysitter.py
@@ -3,6 +3,9 @@ from babysitter import Babysitter
 
 
 class TestBabysitter(unittest.TestCase):
+    def setUp(self):
+        self.b = Babysitter("5p", "9p", "a")
+    
     def test_starts_no_earlier_than_5_pm(self):
-        b = Babysitter("5p", "9p", "a")
-        self.assertEqual(True, b.is_valid_start_time())
+        self.assertEqual(True, self.b.is_valid_start_time())
+
