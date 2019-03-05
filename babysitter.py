af88ad0961a320e9bc1148d5ff61273b6c0838b3
Cisco Garcia <cisco.garcia@helloworld.com>
2019-03-05 13:32:56 -0500

    refactor to use proper boolean assertions

--- babysitter.py
+++ babysitter.py
@@ -7,14 +7,10 @@ class Babysitter():
         self.times = ["5:00pm", "6:00pm", "7:00pm", "8:00pm", "9:00pm", "10:00pm", "11:00pm", "12:00am", "1:00am", "2:00am", "3:00am", "4:00am"]
 
     def is_valid_start_time(self):
-        if self.start_time in self.times:
-            return True
-        return False
+        return self.start_time in self.times
 
     def is_valid_end_time(self):
-        if self.end_time in self.times:
-            return True
-        return False
+        return self.end_time in self.times
 
     def has_not_babysat_tonight(self):
         return self.has_babysat_tonight
--- test_babysitter.py
+++ test_babysitter.py
@@ -7,16 +7,16 @@ class TestBabysitter(unittest.TestCase):
         self.b = Babysitter("5:00pm", "3:00am", "a")
 
     def test_starts_no_earlier_than_5_pm(self):
-        self.assertEqual(True, self.b.is_valid_start_time())
+        self.assertTrue(self.b.is_valid_start_time())
 
     def test_starts_no_later_than_4_am(self):
-        self.assertEqual(True, self.b.is_valid_end_time())
+        self.assertTrue(self.b.is_valid_end_time())
 
     def test_only_babysits_for_1_family__per_night(self):
-        self.assertEqual(False, self.b.has_not_babysat_tonight())
+        self.assertFalse(self.b.has_not_babysat_tonight())
 
     def test_no_fractional_hours(self):
-        self.assertEqual(True, self.b.hours_are_not_fractional())
+        self.assertTrue(self.b.hours_are_not_fractional())
 
     def test_begin_time_is_before_end_time(self):
         self.assertTrue(self.b.begin_time_is_before_end_time())
