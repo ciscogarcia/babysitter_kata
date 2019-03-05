ac652c24d85c0fcca1c9fe940822a5b9d3fa1bcd
Cisco Garcia <cisco.garcia@helloworld.com>
2019-03-05 13:26:23 -0500

    update test to cover all different time periods

--- test_babysitter.py
+++ test_babysitter.py
@@ -4,7 +4,7 @@ from babysitter import Babysitter
 
 class TestBabysitter(unittest.TestCase):
     def setUp(self):
-        self.b = Babysitter("5:00pm", "9:00pm", "a")
+        self.b = Babysitter("5:00pm", "3:00am", "a")
 
     def test_starts_no_earlier_than_5_pm(self):
         self.assertEqual(True, self.b.is_valid_start_time())
