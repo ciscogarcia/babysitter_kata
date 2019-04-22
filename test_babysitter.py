fce95dc7c0f45c0a411e93d2dcd3771f4376d58d
Cisco Garcia <cisco.garcia@helloworld.com>
2019-04-22 13:22:41 -0400

    Test for $15/h before 11pm
    Add test for charging $15/hr before 11pm, additional refactoring

--- test_babysitter.py
+++ test_babysitter.py
@@ -4,29 +4,38 @@ from babysitter import Babysitter
 
 class TestBabysitter(unittest.TestCase):
     def setUp(self):
-        self.b = Babysitter("5:00pm", "3:00am", "a")
+        self.a = Babysitter("5:00pm", "3:00am", "a")
 
     def test_starts_no_earlier_than_5_pm(self):
-        self.assertTrue(self.b.is_valid_start_time())
+        self.assertTrue(self.a.is_valid_start_time())
 
     def test_starts_no_later_than_4_am(self):
-        self.assertTrue(self.b.is_valid_end_time())
+        self.assertTrue(self.a.is_valid_end_time())
 
     def test_only_babysits_for_1_family__per_night(self):
-        self.assertFalse(self.b.has_babysat_tonight())
+        self.assertFalse(self.a.has_babysat_tonight())
 
     def test_no_fractional_hours(self):
-        self.assertTrue(self.b.hours_are_not_fractional())
+        self.assertTrue(self.a.hours_are_not_fractional())
 
     def test_begin_time_is_before_end_time(self):
-        self.assertTrue(self.b.begin_time_is_before_end_time())
+        self.assertTrue(self.a.begin_time_is_before_end_time())
 
     def test_family_is_valid(self):
-        self.assertTrue(self.b.family_is_valid())
+        self.assertTrue(self.a.family_is_valid())
 
     def test_can_calculate_nightly_charge(self):
-        self.assertTrue(self.b.can_calculate_nightly_charge())
+        self.assertTrue(self.a.can_calculate_nightly_charge())
 
+    def test_family_a_charges_15_before_11(self):
+        a = Babysitter("5:00pm", "6:00pm", "a")
+        self.assertEqual(a.nightly_charge(), 15)
+        b = Babysitter("5:00pm", "7:00pm", "a")
+        self.assertEqual(b.nightly_charge(), 30)
+        c = Babysitter("5:00pm", "8:00pm", "a")
+        self.assertEqual(c.nightly_charge(), 45)
+        d = Babysitter("5:00pm", "11:00pm", "a")
+        self.assertEqual(d.nightly_charge(), 90)
 
 if __name__ == '__main__':
     unittest.main()
