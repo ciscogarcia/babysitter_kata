d6f699b42441960cf24af3ffef3261cab095d636
Cisco Garcia <cisco.garcia@helloworld.com>
2019-04-23 09:33:45 -0400

    Implement partial times

--- babysitter.py
+++ babysitter.py
@@ -76,20 +76,20 @@ class Babysitter():
         return hours_before_nine * 21 + hours_after_nine * 15
 
     def calculate_family_b(self):
-        hours_before_10 = None
-        hours_after_12 = None
-        hours_between_10_and_12 = None
+        hours_before_10 = 0
+        hours_after_12 = 0
+        hours_between_10_and_12 = 0
 
         if self.times.index(self.end_time) <= self.times.index("10:00pm"):
             hours_before_10 = self.times.index(self.end_time) - self.times.index(self.start_time)
             return hours_before_10 * 12
-        else:
+        elif self.times.index(self.start_time) < self.times.index("10:00pm"):
             hours_before_10 = self.times.index("10:00pm") - self.times.index(self.start_time)
 
         if self.times.index(self.start_time) >= self.times.index("12:00am"):
             hours_after_12 = self.times.index(self.end_time) - self.times.index(self.start_time)
             return hours_after_12 * 16
-        else:
+        elif self.times.index(self.end_time) >= self.times.index("12:00am"):
             hours_after_12 = self.times.index(self.end_time) - self.times.index("12:00am")
 
         if self.times.index(self.start_time) >= self.times.index("10:00pm") and self.times.index(self.end_time) <= self.times.index("12:00am"):
@@ -98,5 +98,8 @@ class Babysitter():
 
         if self.times.index(self.start_time) <= self.times.index("10:00pm") and self.times.index(self.end_time) >= self.times.index("12:00am"):
             hours_between_10_and_12 = 2
+
+        if self.times.index(self.start_time) <= self.times.index("10:00pm") and self.times.index(self.end_time) > self.times.index("10:00pm") and self.times.index(self.end_time) < self.times.index("12:00am"):
+            hours_between_10_and_12 = 1
 
         return hours_before_10 * 12 + hours_after_12 * 16 + hours_between_10_and_12 * 8
